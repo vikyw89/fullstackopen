29a15b03c314351d6ea2caa2a948fc74a9795804
vikyw89 <vikyw89@gmail.com>
2022-10-17 11:59:12 +0800

    2.5 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -48,11 +48,11 @@ const App = () => {
   ]
 
   return (
-    <>
+    <div>
       {courses.map(course =>
       <Course key={course.id} course={course}/>
       )}
-    </>
+    </div>
   
   )
 }
