7b8fd30538351c99e474204c17d77c07df9daf9f
vikyw89 <vikyw89@gmail.com>
2022-10-17 17:08:37 +0800

    2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1,
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     console.log('addPerson')
@@ -21,7 +22,6 @@ const App = () => {
         return
       }
     }
-    
     const personObject = {
       id: persons.length+1,
       name: newName,
@@ -41,9 +41,37 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+
+  const HandleFilter = (event) => {
+    console.log('HandleFilter')
+    setNewFilter(event.target.value)
+  }
+
+  const filterResult = () => {
+    console.log('filterResult')
+    let temp = []
+    for (var element of persons) {
+      let db = element.name.toLowerCase()
+      if (db.includes(filter.toLowerCase())) {
+        temp.push(element)
+      }
+    }
+    console.log('return', temp)
+    return temp
+  }
+
+  const personsToShow = filterResult()
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <p>
+          filter shown with <input value={filter} onChange={HandleFilter}/>
+        </p>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={HandleNameChange}/>
@@ -57,8 +85,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(element => {
-          console.log('element', element)
+        {personsToShow.map(element => {
           return (
             <div key={element.id}>
               {element.name} {element.number}
