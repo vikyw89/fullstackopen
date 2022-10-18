4373c0c295e7a3c04e58e464f9ae707aee04e56d
vikyw89 <vikyw89@gmail.com>
2022-10-18 14:47:01 +0800

    2.12 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,75 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react';
 
+const Display = ({showCountries}) => {
+  console.log('Display', showCountries)
+  return (
+    <>
+      {showCountries.length === 1
+        ? <ShowDetail showCountries={showCountries}/>
+        : <ShowNames showCountries={showCountries}/>
+      }
+    </>
+  )
+}
+
+const ShowNames = ({showCountries}) => {
+  console.log('ShowNames')
+  return (
+    <>
+      {showCountries.map(element => {
+        return (
+          <div key={element.name.common}>
+            {element.name.common}
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+const ShowDetail = ({showCountries}) => {
+  console.log('ShowDetail')
+  return (
+    <>
+      {showCountries.map(element => {
+        return (
+          <div key={element.name.common}>
+            <h1>
+              {element.name.common}
+            </h1>
+            <p>
+              capital {element.capital[0]}
+              <br></br>
+              area {element.area}
+            </p>
+            <h3>
+              languages :
+            </h3>
+            <Languages language={element.languages}/>
+            <img src={element.flags.png} alt='flags'/>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+const Languages = ({language}) => {
+  console.log('Languages', language)
+  return (
+    <>
+      {Object.values(language).map(element => {
+        return (
+          <li>
+            {element}
+          </li>
+        )
+      })}
+    </>
+  )
+}
+
 function App() {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
@@ -31,38 +100,22 @@ function App() {
   }
 
   const showCountries = countries.filter(showFilter)
-
-  const decideCondition = (props) => {
-    console.log('decideCondition')
-    if (props.length === 1) {
-      return 'detail'
-    }
-    if (props.length > 10) {
-      return 'too many'
-    } else {
-      return 'names'
-    }
-  }
-
-  const condition = decideCondition(countries)
   
-
   return (
     <div>
       <div>
         find countries <input value={filter} onChange={handleFilter}/>
       </div>
       <div>
-        {showCountries.map(element => {
-          return (
-            <div key={element.name.common}>
-              {element.name.common}
-            </div>
-          )
-          })}
+        {showCountries.length > 10
+          ? <div>'Too many matches, specify another filter'</div>
+          : <Display showCountries={showCountries}/>
+        }
       </div>
     </div>
   )
 }
 
+
 export default App;
+
