eb04de48575b4f5a3cf6005f3201559a21989d15
vikyw89 <vikyw89@gmail.com>
2022-10-27 14:57:57 +0800

    3.18 done

--- part2/phonebook/src/components/AddPersons.js
+++ part2/phonebook/src/components/AddPersons.js
@@ -3,29 +3,31 @@ import { useState } from 'react'
 import personsServices from '../services/persons'
 
 const AddPersons = ({persons, setPersons, setNotif}) => {
-    console.log('PersonForm')
-    const [newName, setNewName] = useState('')
-    const [newNumber, setNewNumber] = useState('')
-  
-    const handleNameChange = (event) => {
-      console.log(event.target.value)
-      setNewName(event.target.value)
+  console.log('PersonForm')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    console.log('addPerson')
+    event.preventDefault()
+
+    const newPerson= {
+      name: newName,
+      number: newNumber
     }
-  
-    const handleNumberChange = (event) => {
-      console.log(event.target.value)
-      setNewNumber(event.target.value)
-    }
-  
-    const addPerson = (event) => {
-      console.log('addPerson')
-      event.preventDefault()
-  
-      const newPerson= {
-        name: newName,
-        number: newNumber
-      }
-  
+
+    //check if the same name is available
+    if ((persons.filter( person => person.name === newName).length === 0)) {
       personsServices
         .create(newPerson)
         .then(response => {
@@ -37,22 +39,36 @@ const AddPersons = ({persons, setPersons, setNotif}) => {
             setNotif(null)
           },5000)
         })
+    } else {
+      const [oldPerson] = persons.filter( person => person.name === newName)
+      console.log('line44',oldPerson)
+      personsServices
+        .update(oldPerson.id, newPerson)
+        .then(response => {
+          setPersons(persons.map(person => person.id !== response.id ? person : response))
+          setNotif({success:`${oldPerson.name} already exists in contact, we just updated the phone number`})
+          setNewName('')
+          setNewNumber('')
+          setTimeout(() => {
+            setNotif(null)
+          },5000)
+        })
     }
-    return (
-      <>
-        <form onSubmit={addPerson}>
-          <div>
-            name: <input value={newName} onChange={handleNameChange}/>
-          </div>
-          <div>
-            number: <input value={newNumber} onChange={handleNumberChange}/>
-          </div>
-          <div>
-            <button type="submit" className="btn btn-secondary">add</button>
-          </div>
-        </form>
-      </>
-    )
   }
-
+  return (
+    <>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit" className="btn btn-secondary">add</button>
+        </div>
+      </form>
+    </>
+  )
+}
 export default AddPersons
