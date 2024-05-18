dd002b4a0e3ff39a1c9ad16b2c698d3f6393c4de
vikyw89 <vikyw89@gmail.com>
2024-05-18 16:32:45 +0800

    feat: env

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -4,12 +4,12 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require("dotenv").config()
 const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 
-const mongoUrl = process.env.MONGODB_URL
-
-mongoose.connect(mongoUrl)
+const MONGODB_URL = process.env.NODE_ENV === 'test' 
+  ? process.env.TEST_MONGODB_URL
+  : process.env.MONGODB_URL
+mongoose.connect(MONGODB_URL)
 
 app.use(cors())
 app.use(express.json())
@@ -22,7 +22,5 @@ app.use("/api/blogs", blogsRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+
+module.exports = app
