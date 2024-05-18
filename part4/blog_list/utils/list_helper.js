192b12c653782f4e8918197923f5bdc7672efc66
vikyw89 <vikyw89@gmail.com>
2024-05-18 16:21:21 +0800

    feat: max likes

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -21,8 +21,58 @@ const favoriteBlog = (blogs) => {
     return favouriteBlog
 }
 
+const mostBlogs = (blogs) => {
+    const authorBlogsCount = blogs.reduce((acc, blog) => {
+        if (acc[blog.author]) {
+            acc[blog.author] += 1
+        } else {
+            acc[blog.author] = 1
+        }
+        return acc
+    }, {})
+
+    let maxBlogCount = 0
+    let mostBlogsAuthor
+    for (let author in authorBlogsCount) {
+        if (authorBlogsCount[author] > maxBlogCount) {
+            maxBlogCount = authorBlogsCount[author]
+            mostBlogsAuthor = author
+        }
+    }
+
+    return {
+        author: mostBlogsAuthor,
+        blogs: maxBlogCount
+    }
+}
+const mostLikes = (blogs) => {
+    const authorLikesCount = blogs.reduce((acc, blog) => {
+        if (acc[blog.author]) {
+            acc[blog.author] += blog.likes
+        } else {
+            acc[blog.author] = blog.likes
+        }
+        return acc
+    }, {})
+    
+    let maxLikes = 0
+    let mostLikesAuthor
+    for (let author in authorLikesCount) {
+        if (authorLikesCount[author] > maxLikes) {
+            maxLikes = authorLikesCount[author]
+            mostLikesAuthor = author
+        }
+    }
+
+    return {
+        author: mostLikesAuthor,
+        likes: maxLikes
+    }
+}
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
